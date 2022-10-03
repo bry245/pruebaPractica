1bb5356e1dfcbc42ee41488b3cfd6e905517fbad
melan <melan@192.168.1.102>
2022-10-02 21:11:12 -0600

    Configuración página index, grey´s y station. Se agregaron imagénes.

---
+++ Programación/Web y Patrones/CMtv/src/main/java/com/CMtv/CMtvApplication.java
@@ -0,0 +1,13 @@
+package com.CMtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMtvApplication.class, args);
+	}
+
+}
---
+++ Programación/Web y Patrones/CMtv/src/test/java/com/CMtv/CMtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CMtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CMtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

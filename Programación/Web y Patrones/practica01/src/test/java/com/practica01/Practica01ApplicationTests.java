b65f5775271e82247a1327f31352337dedaa8b6d
melan <melan@192.168.1.102>
2022-10-22 11:41:37 -0600

    Configuración inicial

---
+++ Programación/Web y Patrones/practica01/src/test/java/com/practica01/Practica01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
